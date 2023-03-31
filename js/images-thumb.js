d971d1280432817c8eeb55576fd00001d6bac22c
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-03-31 21:37:32 +0200

    refactor: replace for..of by foreach and event.target by event.currentTarget

--- js/images-thumb.js
+++ js/images-thumb.js
@@ -1,20 +1,23 @@
 const thumbs = {
   imgThumbCurrent: null,
   handleImgThumb: function(event){
-    const imgThumb = event.target; // image sélectionnée
+    const imgThumb = event.currentTarget; // image sélectionnée
     thumbs.imgThumbCurrent = imgThumb.dataset.image; // récup id de l'image sélectionnée
     for (const imgThumb of thumbs.imgThumbs) {
-      imgThumb.classList.remove('image-active'); // init classes
+      imgThumb.parentNode.classList.remove('image-active'); // init classes
     }
     imgThumb.parentNode.classList.add('image-active'); // add active
     thumbs.mainImage.src = `images/image-product-${thumbs.imgThumbCurrent}.jpg`; // ajouter l'image sélectionnée
   },
   init: function(){
     thumbs.mainImage = document.querySelector('.product__image-main img'); // image principale
-    thumbs.imgThumbs = document.querySelectorAll('.product__image-thumbs-container'); // tableau thumbs containers
-    for (const imgThumb of thumbs.imgThumbs) {
+    thumbs.imgThumbs = document.querySelectorAll('.product__image-thumbs-container img'); // tableau thumbs containers
+    // for (const imgThumb of thumbs.imgThumbs) {
+    //   imgThumb.addEventListener('click', thumbs.handleImgThumb); // ajout écouteur onclick sur thumb
+    // }
+    thumbs.imgThumbs.forEach(imgThumb => {
       imgThumb.addEventListener('click', thumbs.handleImgThumb); // ajout écouteur onclick sur thumb
-    }
+    });
   },
 };
 document.addEventListener('DOMContentLoaded', thumbs.init);
