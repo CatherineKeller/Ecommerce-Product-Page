fd743993fca1eca5b7038d3b83d72d51ab3e9447
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-04-06 17:27:17 +0200

    feat: add responsive (menu...)

--- js/header.js
+++ js/header.js
@@ -1,9 +1,11 @@
 const header = {
   toggleMenu: function(){
     header.menu.classList.toggle('menu-active');
+    header.overlay.classList.toggle('active');
   },
   init: function(){
     header.menu = document.getElementById('menu'); // Menu button
+    header.overlay = document.getElementById('overlay');
     
     // On menu btn
     header.menu.addEventListener('click', header.toggleMenu);
